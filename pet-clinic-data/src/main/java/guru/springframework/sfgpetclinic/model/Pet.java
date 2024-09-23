e2655e335fb9fe019976767df795cbf233bcde48
Wojciech Koczwara <61041670+wojtekkoczwara@users.noreply.github.com>
2024-09-23 12:03:49 +0200

    pet type on startup CLOSES #48

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -3,6 +3,8 @@ package guru.springframework.sfgpetclinic.model;
 import java.time.LocalDate;
 
 public class Pet  extends BaseEntity{
+
+    private String name;
     private LocalDate birthDate;
     private Owner Owner;
     private PetType petType;
@@ -29,5 +31,13 @@ public class Pet  extends BaseEntity{
 
     public void setPetType(PetType petType) {
         this.petType = petType;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
